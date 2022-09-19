5b7c40584bb93a7a8f56960ed9e9b1ac245e34ac
javierdebug <99869456+javierdebug@users.noreply.github.com>
2022-09-18 21:37:00 -0500

    Add solution in TypeScript

---
+++ Solutions/algorithm-exercise-Avengers-tickets.ts
@@ -0,0 +1,45 @@
+const checkCashRegister = (clientBill : number[]) : string => {
+  let currentBills : {hundred : number, fifty : number, twentyFive: number} = {
+    hundred: 0,
+    fifty: 0,
+    twentyFive: 0,
+  };
+
+  for (let i = 0; i < clientBill.length; i++) {
+    if (clientBill[i] === 25) {
+      currentBills.twentyFive++;
+      continue;
+    }
+    if (clientBill[i] === 50) {
+      if (currentBills.twentyFive > 0) {
+        currentBills.twentyFive--;
+        currentBills.fifty++;
+        continue;
+      } else {
+        return "NO";
+      }
+    }
+    if (clientBill[i] === 100) {
+      if (currentBills.fifty > 0 && currentBills.twentyFive > 0) {
+        currentBills.fifty--;
+        currentBills.twentyFive--;
+        currentBills.hundred++;
+        continue;
+      } else if (currentBills.twentyFive > 0) {
+        currentBills.twentyFive -= 3;
+        currentBills.hundred++;
+      } else {
+        return "NO";
+      }
+    }
+    if (currentBills.twentyFive < 0 || currentBills.fifty < 0) return "NO";
+  }
+
+  return "YES";
+};
+
+//Some tests:
+console.log(checkCashRegister([25, 25, 50]));
+console.log(checkCashRegister([25, 100]));
+console.log(checkCashRegister([25, 25, 50, 50, 50]));
+console.log(checkCashRegister([50, 25, 50]));
